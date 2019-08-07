0f0400fb6cd7c2e5daa80061e4c13f87216c5c0c
Alicannklc <alicannklc@gmail.com>
2019-08-07 06:20:19 +0300

    update taılwind 1.1.0 xml

--- src/main/java/com/alicannklc/tailwind/TailwindTemplateProvider.java
+++ src/main/java/com/alicannklc/tailwind/TailwindTemplateProvider.java
@@ -5,9 +5,7 @@ import org.jetbrains.annotations.Nullable;
 
 public class TailwindTemplateProvider implements DefaultLiveTemplatesProvider {
    public String[] getDefaultLiveTemplateFiles() {
-      return new String[]{"liveTemplates/Tailwind",
-                          "liveTemplates/Tailwindcss-1_0_0" 
-                         };
+      return new String[]{"liveTemplates/Tailwindcss-1_1_0"};
    }
 
    @Nullable
